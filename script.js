7bc93f8907efdda4b34b185419d0bf83a4bfbffd
keshejiuli <728658698@qq.com>
2025-03-05 22:37:53 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// 示例：动态修改页面内容
-document.addEventListener('DOMContentLoaded', function () {
-    const hobbies = document.querySelector('.hobbies ul');
-    hobbies.innerHTML += '<li>学习新技术</li>'; // 动态添加一个爱好
-});
